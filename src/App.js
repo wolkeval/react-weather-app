330063b5bc4c98a336c6b036ffd4cedc962e4b72
wolkeval <68792665+wolkeval@users.noreply.github.com>
2020-10-11 22:14:28 +0200

    Revert "Imported weather app"
    This reverts commit 289be59c030233830b151cf663b1db47973f717d.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import "./App.css";
-
-import Header from "./Header";
-import Main from "./Main";
-import Footer from "./Footer";
-
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <div className="container">
-        <Header />
-        <Main />
-        <Footer />
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
+export default App;
